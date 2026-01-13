4d5efecb32883c7d51011b66fab447b229330e91
MaxDevNet <sabanoff.maksim@gmail.com>
2026-01-13 18:25:07 +0500

    DS-B-2 done

---
+++ DirectoryService/src/DirectoryService.Presentation/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Directory Service API",
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
